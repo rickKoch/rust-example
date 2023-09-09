91ab1883f22d075158e9ee45b630df68532c1ef2
kochevRisto <kochev.risto@gmail.com>
2023-09-09 23:25:19 +0200

    add unit and integration tests

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -3,6 +3,8 @@ use actix_web::{
     HttpResponse,
 };
 
+pub const API_VERSION: &str = "v0.0.1";
+
 pub fn service(cfg: &mut ServiceConfig) {
     // cfg.service(health);
     cfg.route("/health", web::get().to(health));
@@ -10,6 +12,24 @@ pub fn service(cfg: &mut ServiceConfig) {
 
 async fn health() -> HttpResponse {
     HttpResponse::Ok()
-        .append_header(("version", "0.0.1"))
+        .append_header(("version", API_VERSION))
         .finish()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+
+    #[actix_rt::test]
+    async fn health_check_works() {
+        let res = health().await;
+
+        assert!(res.status().is_success());
+        assert_eq!(res.status(), StatusCode::OK);
+
+        let data = res.headers().get("version").and_then(|v| v.to_str().ok());
+
+        assert_eq!(data, Some(API_VERSION));
+    }
 }
